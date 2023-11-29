88677a449f19df2df02358fe291dcc1b6b47d301
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-11-29 11:43:43 +0100

    Changed data, added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Declares all of the imports needed for this to work
 const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
@@ -5,6 +6,7 @@ const { Server } = require("socket.io");
 const fs = require("fs");
 const fileUpload = require("express-fileupload");
 
+// Creates a server using express
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
@@ -13,35 +15,40 @@ const io = new Server(server);
 // Declares some of the needed rules for the server, like static changes all img dir to the main div
 // as to not have to send every image
 app.use(fileUpload());
-
 app.use(express.static("public"));
-
 app.use(express.static(__dirname + "/upload"));
 
-
+// When starting the server and on the path "/" send the file index.html to the site
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "public/index.html"));
 });
 
+// On a post request on the path "/" get the data and send it to the userData.json file as json format
 app.post("/", (req, res) => {
   var userData = fs.readFileSync("userData.json");
   let alreadyExists = false;
   var myObj = JSON.parse(userData);
+  // Loops through all users and checks if username is taken
   myObj.Users.map((user) => {
     if (req.body.username == user.username) {
       alreadyExists = true;
+      console.log("Username taken!");
+      res.sendFile(join(__dirname, "public/index.html"));
+
+      // Does not work
+      /* io.emit("Already exists"); */
     }
   });
   if (!alreadyExists) {
-    // Move the uploaded image to our upload folder
+    // Move the uploaded image to upload folder
     req.files.image.mv(__dirname + "/upload/" + req.files.image.name);
 
-    // All good
     console.log("Image was uploaded!");
 
     delete req.body.submit;
     req.body.image = req.files.image.name;
 
+    // Takes the input from the form and sends it to the json file as styled json data
     myObj.Users.push(req.body);
     var newData2 = JSON.stringify(myObj, null, 2);
     fs.writeFile("userData.json", newData2, (err) => {
@@ -49,13 +56,12 @@ app.post("/", (req, res) => {
       console.log("New data added");
     });
     res.sendFile(join(__dirname, "public/index.html"));
-  }else{
-    res.sendFile(join(__dirname, "public/index.html"));
-    io.emit("Already exists");
   }
 });
 
+// Runs when a connection is made between user and server
 io.on("connection", (socket) => {
+  // Runs when "edit" is sent to the server. It edits a user by selecting them by their username and modifying them by the data sent in
   socket.on("edit", (data) => {
     var userData = fs.readFileSync("./userData.json");
 
@@ -67,12 +73,14 @@ io.on("connection", (socket) => {
       console.log("New data added");
     });
   });
+  // Runs when "show" is sent to the server. Sends all of the users to the index.html file to get printed out
   socket.on("show", (data) => {
     var userData = fs.readFileSync("./userData.json");
 
     var myObj = JSON.parse(userData);
     io.emit("printAll", { Users: myObj });
   });
+  // Runs when "searchFor" is sent to the server. Gets the users data by the sent in username, then sends it to the index.html for using
   socket.on("searchFor", (data) => {
     let i = false;
     var userData = fs.readFileSync("./userData.json");
@@ -88,6 +96,7 @@ io.on("connection", (socket) => {
       io.emit("this user", false);
     }
   });
+  // Runs when "change this user" is sent to the server. Creates a object with an array, then pushes all users plus the modified user to it then writes it to the .json file
   socket.on("change this user", (who) => {
     let arr = { Users: [] };
     var userData = fs.readFileSync("./userData.json");
@@ -101,10 +110,6 @@ io.on("connection", (socket) => {
         user["proffesion"] = who.proffesion;
 
         // It is not able to edit which image due to post issues with uploading images
-        // console.log(who.image);
-        // if (who.image != null) {
-        //   //user["image"] = who.image;
-        // }
       }
       arr.Users.push(user);
     });
@@ -115,6 +120,7 @@ io.on("connection", (socket) => {
   });
 });
 
+// Starts the server at port 3000
 server.listen(3000, () => {
   console.log("Server is listening at port 3000");
 });
